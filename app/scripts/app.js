a2105554fe9c5de54dd9c98434b943dbc6c68bef
Samuli Ulmanen <samuli.ulmanen@efecte.fi>
2013-05-15 17:43:07 +0300

    DEVEL: yeoman project.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var hjspeer3App = angular.module('hjspeer3App', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+hjspeer3App.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
